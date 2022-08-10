4b3840a77a3ff3ccdd937d5de363fbd6998119c3
阿冰 <3398341353@qq.com>
2022-08-10 12:30:17 +0800

    初始化并提交

---
+++ cmd/chatroom/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "net/http/pprof"
+
+	"github.com/polaris1119/chatroom/global"
+	"github.com/polaris1119/chatroom/server"
+)
+
+var (
+	addr   = ":2022"
+	banner = `
+    ____              _____
+   |    |    |   /\     |
+   |    |____|  /  \    | 
+   |    |    | /----\   |
+   |____|    |/      \  |		ChatRoom，start on：%s
+`
+)
+
+func init() {
+	global.Init()
+}
+
+func main() {
+	fmt.Printf(banner, addr)
+
+	server.RegisterHandle()
+
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
